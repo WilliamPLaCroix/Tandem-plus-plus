92d257a4c84d2105b1496e92cce461b4dad4a6b7
WilliamPLaCroix <williamplacroix@gmail.com>
2023-05-20 16:46:43 +0200

    found a corpus with spoken dialogue and parsed

--- data_tokeniser.py
+++ data_tokeniser.py
@@ -1,8 +1,12 @@
+import re
+import string
+
 import pandas as pd
+import textgrid as tg  # https://github.com/nltk/nltk_contrib/blob/master/nltk_contrib/textgrid.py
 from nltk.tokenize import word_tokenize
 
 
-def tokenize_dataset() -> list:
+def tokenize_cefr_texts() -> list:
     """
     Grouped data structure format:
     groups[group number][column][row]
@@ -13,11 +17,27 @@ def tokenize_dataset() -> list:
     pulls first text from A1 group
     """
     with open("./cefr-dataset/data.csv", "r", encoding="utf-8") as file:
-        groups = [
-            gb.get_group(group)  # ignore the linter error, it's just jealous.
-            for group in pd.read_csv(file, index_col=0)  # df with columns CEFR, Text
-            .applymap(word_tokenize)  # tokenize each text
-            .groupby("CEFR")  # separate into [A1, A2, B1, B2, C1, C2]
-            .groups  # pull group items
-        ]
+        gb = pd.read_csv(file, index_col=0).applymap(word_tokenize).groupby("CEFR")
+        groups = [gb.get_group(group) for group in gb.groups]
     return groups
+
+
+def tokenize_textgrid(filename) -> list:
+    """
+    Reads in TextGrid file, extracting transcript from grid.tiers[0]
+    cleans off [] from (sic?) words,
+    returns tokenized list >>> no punctuation, <P> indicates pause
+    """
+    pattern = re.compile("[\[\]]+")
+    fp = open(filename, "r", encoding="utf-8").read()
+    grid = tg.TextGrid(fp)
+    tier_0 = grid.tiers[0]
+    full_text = " ".join(
+        [
+            text
+            for (start, stop, text) in tier_0.simple_transcript
+            if text != "<HÄSITATION>"
+        ]
+    )
+    cleaned = pattern.sub("", full_text).split()
+    return cleaned
